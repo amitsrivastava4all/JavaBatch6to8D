e56d3e90779fdfcd7afd8f084e93a455c7cec100
amit4alljava <amit4alljava@gmail.com>
2016-09-20 19:51:23 +0530

    Code

---
+++ FirstClass.java
@@ -0,0 +1,54 @@
+/*
+Procedural Programming
+Divide a Big Problem into small small procedures
+and each procedure is actually a function
+C - Programmer they divide a problem into functions
+so they think only functions , C Doesn't think about
+data
+
+
+OOP - Think about data and functions
+
+Class - 
+
+
+class Calc{
+int x,y; // Member  Variables
+int add(){  // Member Functions
+return x + y;
+}
+
+int subtract(){
+return x - y;
+}
+}
+class Customer
+{
+int x;
+}
+*/
+class FirstClass
+{
+    // public - now u can access your things outside the package (folder)
+    // static - it has only one copy in the memory
+    // void - no return 
+    // main - entry point for application
+    // String - it is a predefine class in Java
+    // String is a char array internally
+    // String it is a collection of chars
+    // args - it is just a variable and  name can be anything
+    // args it is of array type 
+    // String args[] - command line arguments
+    
+    public static void main(String arr[]){
+        // System is a predefine class
+        // and out is a predefine object
+        // out is a object of PrintStream class
+        // PrintStream has println method
+        // println is used to print on console 
+        // and it print in a new line
+        System.out.println("Hello Java !");    
+    }
+}
+
+
